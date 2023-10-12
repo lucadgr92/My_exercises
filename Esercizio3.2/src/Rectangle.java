62b30a41a6a69c7e084cddc427ac4be489fb3cf4
Luca Di Grigoli <lucadgr92@gmail.com>
2023-10-12 22:17:16 +0200

    Esercizio3.2 - Seconda versione.

--- Esercizio3.2/src/Rectangle.java
+++ Esercizio3.2/src/Rectangle.java
@@ -1,13 +1,7 @@
 public class Rectangle extends Shape{
-    double width;
-    double height;
-
     public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
+        super(width, height);
     }
-
-
     @Override
     public double calcolaArea() {
         return this.height * this.width;
--- Esercizio3.2/src/Shape.java
+++ Esercizio3.2/src/Shape.java
@@ -1,5 +1,11 @@
 public class Shape {
-  public double calcolaArea() {
+    double width;
+    double height;
+    public Shape(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public double calcolaArea() {
       double area = 0;
       return area;
   }
