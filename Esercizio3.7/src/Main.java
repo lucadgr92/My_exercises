6c3e36cbfd32a1499a60e63f953086e38dde71b8
Luca Di Grigoli <lucadgr92@gmail.com>
2023-10-14 22:41:14 +0200

    Esercizio3.7 - Prima versione.

---
+++ Esercizio3.7/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+    stringProcessor("Ottogenario", 37);
+    }
+public static void stringProcessor(String string, int index) {
+        StringBuilder hexaDecimal = new StringBuilder();
+        if(index > string.length()) {
+            System.out.println("Errore: indice non valido.");
+        } else {
+           char char1 = string.charAt(index);
+           int decimalCode = Character.codePointAt(new char[] {char1}, 0);
+           hexaDecimal.append(Integer.toHexString(decimalCode).toUpperCase());
+           System.out.println("Il codice unicode di " + char1 + " è U+" + hexaDecimal);
+        }
+}
+}
+
+    //Scrivi un programma Java che abbia un metodo che prenda il
+    //carattere in Unicode in un indice specifico all'interno di una
+    //stringa e lo stampi.
+
+    //Il metodo dovrà avere nel costruttore una stringa ed una posizione e
+    //dovrà resituire il carattere in unicode o una stringa di errore.
+    //Per la creazione della stringa di risultato dovrebbe essere utilizzato
+    //StringBuilder.
