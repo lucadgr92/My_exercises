050130ee82fc73b71167c9ef1f181db6a5303ddd
Luca Di Grigoli <lucadgr92@gmail.com>
2023-11-02 15:52:08 +0100

    Esercizio 2.5 - Seconda versione.

--- Esercizio2.5/src/Main.java
+++ Esercizio2.5/src/Main.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Punto puntoA = new Punto(4,7);
+
+        Map<String, Integer> hashMap1 = new HashMap<>();
+
+        hashMap1.put("Norma", 32);
+        hashMap1.put("Eleonora", 26);
+        hashMap1.put("Margherita", 28);
+        hashMap1.put("Marta", 29);
+
+        ArrayList<Integer> age = new ArrayList<>();
+        for(Integer value : hashMap1.values()) {
+            age.add(value);
+        }
+
+        Collections.sort(age);
+
+        System.out.println(age);
+
 
     }
 }
-
-//Definisci un oggetto Record che rappresenti un Punto con le coordinate x & y
+//Creare un hashmap prendere tutti i valori, ordinarli e stamparli
--- Esercizio2.5/src/Punto.java
+++
@@ -1,2 +0,0 @@
-public record Punto(int x, int y) {
-}
