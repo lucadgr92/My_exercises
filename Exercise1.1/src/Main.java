4cd65523de766404afea978a6d3ca217b3e10c7d
Luca Di Grigoli <lucadgr92@gmail.com>
2023-11-11 00:00:39 +0100

    Esercizi eccezioni - Terza versione.

--- Exercise1.1/src/Main.java
+++ Exercise1.1/src/Main.java
@@ -1,22 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(rangeChecker(8));
+        try {
+            System.out.println(rangeChecker(8));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
     }
 
     public static boolean rangeChecker (int arg1) {
-        try {
-
             if (arg1 < 2 || arg1 > 10) {
                 throw new IllegalArgumentException();
+            } else {
+                return true;
             }
-
-        } catch (IllegalArgumentException e) {
-            System.out.println("Il numero inserito come argomento non rientra nel range consentito.");
-            return false;
-        }
-        return true;
-        }
     }
+}
 
 
 //Scrivere una funzione che controlli se un numero int è in un determinato range
--- Exercise1.2/src/Main.java
+++ Exercise1.2/src/Main.java
@@ -1,43 +1,40 @@
 public class Main {
     public static void main(String[] args) throws NotANumberException {
-    System.out.println(numberChecker('r'));
-    System.out.println(numberChecker2('r', 48, 57));
 
-    //Ho adottato tutti i suggerimenti avanzati nel secondo metodo, mentre nel primo
-    //ho solamente aggiunto il blocco try-catch. Però perché la necessità di passare
-    //gli interi necessari alla verifica come argomenti? La loro funzione era solamente
-    //verificare se il valore unicode del carattere rientrasse entro il range tra 48
-    //e 57 che identifica i numeri da 0 a 9. Il valore decimale che identifica i numeri
-    //è sempre lo stesso ai fini della verifica. Quindi perché permetterne la modifica?
+        try {
+            System.out.println(numberChecker('r'));
+            System.out.println(numberChecker2('r', 48, 57));
+        } catch (NotANumberException e) {
+            System.out.println("Errore: il carattere inserito non è una lettera");
+        }
+
+        //Ho adottato tutti i suggerimenti avanzati nel secondo metodo, mentre nel primo
+        //ho solamente aggiunto il blocco try-catch. Però perché la necessità di passare
+        //gli interi necessari alla verifica come argomenti? La loro funzione era solamente
+        //verificare se il valore unicode del carattere rientrasse entro il range tra 48
+        //e 57 che identifica i numeri da 0 a 9. Il valore decimale che identifica i numeri
+        //è sempre lo stesso ai fini della verifica. Quindi perché permetterne la modifica?
 
     }
 
-public static boolean numberChecker (char arg1) throws NotANumberException {
-        int unicodeDecimal = (int)arg1;
-
-        try {
-            if (unicodeDecimal < 48 || unicodeDecimal > 57) {
-                throw new NotANumberException();
-            }
-        } catch (NotANumberException e) {
-            System.out.println("Il carattere inserito non è un numero.");
-            return false;
+    public static boolean numberChecker(char arg1) throws NotANumberException {
+        int unicodeDecimal = (int) arg1;
+        if (unicodeDecimal < 48 || unicodeDecimal > 57) {
+            throw new NotANumberException();
+        } else {
+            return true;
         }
-        return true;
-}
+    }
 
-    public static boolean numberChecker2 (char arg1, int arg2, int arg3) throws NotANumberException {
-        int unicodeDecimal = (int)arg1;
 
-        try {
+    public static boolean numberChecker2(char arg1, int arg2, int arg3) throws NotANumberException {
+        int unicodeDecimal = (int) arg1;
+
             if (unicodeDecimal < arg2 || unicodeDecimal > arg3) {
                 throw new NotANumberException();
+            } else {
+                return true;
             }
-        } catch (NotANumberException e) {
-            System.out.println("Il carattere inserito non è un numero.");
-            return false;
-        }
-        return true;
     }
 
 }
